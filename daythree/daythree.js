69995f06f0412a63908a3f4b92b7e57c3a1fea6e
shitalsalve3112 <124130106+shitalsalve3112@users.noreply.github.com>
2023-04-24 17:10:57 +0530

    commite two

---
+++ daythree/daythree.js
@@ -0,0 +1,58 @@
+//single line comment
+/*multiline comment */
+/*alert("Congratulations!")
+alert("Hello")
+alert(4+2)
+let num=12;
+alert(num)
+
+var num1=65;
+alert(num1)
+
+const num2=45;
+alert(num2)
+num2 = 20
+alert(num2)*/
+
+//assignment operator
+/*var a = 2
+var b = 5
+alert(a*=b)
+
+let e = 30
+let f = 34
+alert(e/=f)
+*/
+
+//relational operator
+/*var res = 12>0
+alert(res)*/
+
+//logical operator
+/*var x = 10
+var y = 35
+alert(x>=10 && y==35)
+
+var x = 10
+var y = 35
+alert(x==y || y==5)
+
+var x = 10
+var y = 35
+alert(!(x==y))*/
+
+//loop
+/*let p =3, q=2
+if(p>q)
+    alert("Shital")
+else
+    alert("Moule")*/
+
+/*alert(typeof (54))  //number
+alert(typeof ("shital"))    //string
+alert(typeof (true))    //boolean*/
+
+//prompt is used to take input from user
+/*let age=prompt("What's your age??")
+alert("Your age is " +age)*/
+
